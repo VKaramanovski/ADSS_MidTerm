2c709153fab82534e54b14c5ea1d2005f22ad553
VKaramanovski <56731909+VKaramanovski@users.noreply.github.com>
2019-11-15 20:43:15 +0100

    i ovaa

--- ADSS_MidTerm_E4/Program.cs
+++ ADSS_MidTerm_E4/Program.cs
@@ -3,10 +3,44 @@
 namespace ADSS_MidTerm_E4
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            int a, b, c;
+
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+            c = Int32.Parse(Console.ReadLine());
+
+            Triangle triangle = new Triangle(a, b, c);
+
+            isRight(triangle);
+            
+        }
+
+        public static void isRight(Triangle triangle)
+
+
+        {
+            float a, b, c;
+            a = Math.Pow(triangle.a, 2);
+            b = Math.Pow(triangle.b, 2);
+            c = Math.Pow(triangle.c, 2);
+
+            if ( c == a + b)
+            {
+                Console.WriteLine("The triangle is Right");
+                Console.WriteLine("Perimetar:{0} ", triangle.a + triangle.b + triangle.c);
+            }
+
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+
+
         }
     }
+    
 }
---
+++ ADSS_MidTerm_E4/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ADSS_MidTerm_E4
+{
+    public class Triangle
+    {
+
+         int a;
+         int b;
+         int c;
+
+
+
+        public Triangle(int x, int y, int z)
+        {
+            a = x;
+            b = y;
+            c = z;
+
+        }
+
+    }
+}
+
